0242e0610e2d873a2f20a7610ba5e0f69e50eb9f
Gabriel Zurita <gabezurita00@gmail.com>
2020-11-03 19:49:11 -0400

    feat: delete pictures *apparently not working!!*

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -5,22 +5,24 @@
       <div class="contenido">
         <div class="imagenes">
           <h2>Fotos del producto:</h2>
-          <input type="file" multiple name="image" @change="GetImage" />
-          <br/>
-          <img
-            v-for="(value, index) in avatar"
-            :key="index"
-            :src="value"
-            alt="Image"
-            width="100px"
-          /><br />
-          <span v-if="!$v.avatar.maxLength" class="error"
-          >No se aceptan mas de 4 imágenes</span
-          >
-          <span v-if="$v.avatar.minLength" class="error"
-          >Se necesita al menos una imagen</span
-          >
+          <br>
+          <input type="file" id="files" ref="files" accept="image/*" multiple v-on:change="handleFilesUpload()" />
+          <div class="imagenes">
+            <div class="grid-x">
+              <div v-for="(file, key) in files" :key="file" class="file-listing">
+                {{ file.name }}
+                <img class="preview" v-bind:ref="'image'+parseInt( key )"/>
+                <span class="remove-file" v-on:click="removeFile(key)">Remove</span>
+              </div>
+            </div>
+          </div>
+          <br>
+          <div class="imagenes">
+            <button v-on:click="addFiles()">Add Files</button>
+          </div>
+          <br>
         </div>
+        
         <div>
           <h2>Datos del producto:</h2>
           <form
@@ -237,7 +239,7 @@ export default {
   name: "Formulario",
   data() {
     return {
-      avatar: [],
+      files: [],
       producto: {
         nombre_prod: null,
         descripcion: null,
@@ -284,7 +286,7 @@ export default {
         validate_date,
       },
     },
-    avatar: {
+    files: {
       minLength: minLength(1),
       maxLength: maxLength(4),
     }
@@ -293,7 +295,7 @@ export default {
     send: function (event) {
       event.preventDefault();
       console.log(this.producto);
-      console.log(this.avatar);
+      console.log(this.files);
     },
     clickable() {
       // if somethin
@@ -315,17 +317,30 @@ export default {
         console.log("datos incorrectos");
       }
     },
-    GetImage(e) {
-      const amount = e.target.files.length;
-      for (var i = 0; i < amount; i++){
-        const image = e.target.files[i];
-        let reader = new FileReader();
-        reader.readAsDataURL(image);
-        reader.onload = (e) => {
-          this.avatar.push(e.target.result);
-        };
+    addFiles(){
+      this.$refs.files.click();
+    },
+    handleFilesUpload(){
+      let uploadedFiles = this.$refs.files.files;
+      for( var i= 0; i < uploadedFiles.length; i++ ){
+        this.files.push( uploadedFiles[i] );
+      }
+      this.getImagePreviews();
+    },
+    getImagePreviews(){
+      for (let i = 0; i < this.files.length; i++){
+        if ( /\.(jpe?g|png|gif)$/i.test( this.files[i].name ) ){
+          let reader = new FileReader();
+          reader.addEventListener("load", function(){
+            this.$refs['image'+parseInt( i )][0].src = reader.result;
+          }.bind(this), false);
+          reader.readAsDataURL(this.files[i]);
+        }
       }
     },
+    removeFiles( key ){
+      this.files.splice(key, 1);
+    }
   },
 };
 </script>
@@ -436,5 +451,17 @@ span {
 #preview img {
   max-width: 100%;
   max-height: 500px;
+}
+input[type="file"]{
+  position: absolute;
+  top: -500px;
+}
+div.file-listing img{
+  max-width: 90%;
+}
+span.remove-file{
+  color: red;
+  cursor: pointer;
+  float: right;
 }
 </style>
